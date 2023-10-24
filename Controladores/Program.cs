a40d4dd6f2b6cafe29320f42903bcecc8cb86289
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2023-10-24 11:21:56 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,26 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Ejercicio01Condicionales.Servicios;
+
+namespace Ejercicio01Condicionales;
+
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        MenuInterfaz nu = new MenuImplementacion();
+        int valor01 = nu.solicitarNumero();
+
+
+        if ((valor01%2)==0)
+        {
+            Console.WriteLine("El valor el Par");
+        }
+
+        else
+        {
+            Console.WriteLine("El valor es Impar");
+        }
+
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01Condicionales.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+
+
+        public int solictarNumero()
+        {
+            int valor;
+            Console.WriteLine("Indique un Valor Numerico");
+            valor = Convert.ToInt32(Console.ReadLine());
+            return valor;
+        }
+
+
+    }
+}
